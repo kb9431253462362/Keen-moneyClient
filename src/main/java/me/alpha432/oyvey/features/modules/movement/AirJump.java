5f1b8d8cd6ef74f36c77f0d20e852c3c36ad92ca
kb9431253462362 <keenbase9@gmail.com>
2025-12-07 18:52:27 -0500

    Create AirJump.java

---
+++ src/main/java/me/alpha432/oyvey/features/modules/movement/AirJump.java
@@ -0,0 +1,27 @@
+package me.alpha432.oyvey.features.modules.movement;
+
+import me.alpha432.oyvey.features.modules.Module;
+
+public class AirJump extends Module {
+
+    public AirJump() {
+        super("AirJump", "Allows jumping while airborne.", Category.MOVEMENT);
+    }
+
+    @Override
+    public void onTick() {
+        if (mc.player == null) return;
+
+        if (mc.options.keyJump.isDown()) {
+            
+            if (!mc.player.onGround()) {
+                
+                mc.player.onGround = true; 
+                
+                mc.player.jumpFromGround();
+                
+                mc.player.onGround = false;
+            }
+        }
+    }
+}
