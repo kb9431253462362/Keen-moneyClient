661e37f75195f88a0b5c2ec6bb61519ba641f963
kb9431253462362 <keenbase9@gmail.com>
2025-12-08 16:23:48 -0500

    Update CommandManager.java

--- src/main/java/me/alpha432/oyvey/manager/CommandManager.java
+++ src/main/java/me/alpha432/oyvey/manager/CommandManager.java
@@ -15,7 +15,7 @@ import java.util.List;
 public class CommandManager
         extends Feature implements Jsonable {
     private final List<Command> commands = new ArrayList<>();
-    private String clientMessage = "<OyVey>";
+    private String clientMessage = "<Keen-moneyClient>";
     private String prefix = ".";
 
     public CommandManager() {
