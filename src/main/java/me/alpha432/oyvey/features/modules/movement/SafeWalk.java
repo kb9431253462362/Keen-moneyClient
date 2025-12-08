b84ec68ce2dfcbc8af2a85318d65ae4679c83831
kb9431253462362 <keenbase9@gmail.com>
2025-12-07 22:27:55 -0500

    Create SafeWalk.java

---
+++ src/main/java/me/alpha432/oyvey/features/modules/movement/SafeWalk.java
@@ -0,0 +1,28 @@
+package me.alpha432.oyvey.features.modules.movement;
+
+import me.alpha432.oyvey.features.modules.Module;
+import net.minecraft.world.level.Level;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.phys.Vec3;
+
+public class SafeWalk extends Module {
+    
+    public SafeWalk() {
+        super("SafeWalk", "Prevents you from walking off edges.", Category.MOVEMENT);
+    }
+
+    @Override
+    public void onTick() {
+        if (nullCheck()) return;
+        
+        Vec3 forward = mc.player.getForward();
+        BlockPos nextBlockPos = mc.player.getOnPos().offset((int) forward.x, -1, (int) forward.z);
+        
+        Level level = mc.level;
+        if (level.isEmptyBlock(nextBlockPos) && mc.player.onGround()) {
+             if (mc.options.keyForward.isDown()) {
+                 mc.options.keyForward.setDown(false);
+             }
+        }
+    }
+}
